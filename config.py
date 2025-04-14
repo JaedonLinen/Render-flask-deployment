8766c368e3207e2241e0e41b6fd1ed38b41fe77a
Jaedon Linen <jaedonlinen@Jaedons-MacBook-Pro.local>
2025-04-14 14:10:45 -0400

    change db to postgresSQL

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
 
-
+load_dotenv()  # Only needed locally
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///LedgerPro2.0.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
